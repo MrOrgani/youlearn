c67c427fa1a57241f6aa6ceb2ef1876a312148b6
Maxime Organi <maxime.organi@gmail.com>
2023-12-24 09:13:14 +0100

    adding PriceForm

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/PriceForm.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { formatPrice } from "@/lib/format";
+import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Course } from "@prisma/client";
+import axios from "axios";
+import { PenIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import * as z from "zod";
+
+interface PriceFormProps {
+  initialData: Course;
+  courseId: string;
+}
+
+const formSchema = z.object({
+  price: z.coerce.number(),
+});
+
+export const PriceForm = ({ initialData, courseId }: PriceFormProps) => {
+  const [isEdditing, setIsEdditing] = useState(false);
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { price: initialData?.price || undefined },
+  });
+
+  const toggleEdditing = () => setIsEdditing((prev) => !prev);
+
+  const { isValid, isSubmitting } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values);
+      toggleEdditing();
+      toast.success("Course updated");
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <div className="mt-6 rounded-md border bg-slate-100 p-4">
+      <div className="flex items-center justify-between font-medium">
+        Course price
+        <Button variant={"ghost"} onClick={toggleEdditing}>
+          {isEdditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <PenIcon className="mr-2 h-4 w-4" />
+              Edit price
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEdditing && (
+        <p
+          className={cn(
+            "mt-2 text-sm",
+            !initialData.price && "italic text-slate-500",
+          )}
+        >
+          {initialData.price ? formatPrice(initialData.price) : "No price"}
+        </p>
+      )}
+      {isEdditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="mt-4 space-y-4"
+          >
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="number"
+                      placeholder="Price"
+                      step={0.01}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Button type="submit" disabled={!isValid || isSubmitting}>
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
