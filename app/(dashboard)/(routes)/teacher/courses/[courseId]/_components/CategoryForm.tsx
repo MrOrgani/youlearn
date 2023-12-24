3d1a50493829e5f84e62806831629169f0804db4
Maxime Organi <maxime.organi@gmail.com>
2023-12-24 08:58:23 +0100

    Adding category form

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/CategoryForm.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Combobox } from "@/components/ui/combobox";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Course } from "@prisma/client";
+import axios from "axios";
+import { PenIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import * as z from "zod";
+
+interface CategoryFormProps {
+  initialData: Course;
+  courseId: string;
+  options: Array<{ value: string; label: string }>;
+}
+
+const formSchema = z.object({
+  categoryId: z
+    .string()
+    .min(1, { message: "There must be at least one category" }),
+});
+
+export const CategoryForm = ({
+  initialData,
+  courseId,
+  options,
+}: CategoryFormProps) => {
+  const [isEdditing, setIsEdditing] = useState(false);
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { categoryId: initialData?.categoryId || "" },
+  });
+
+  const toggleEdditing = () => setIsEdditing((prev) => !prev);
+
+  const { isValid, isSubmitting } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values);
+      toggleEdditing();
+      toast.success("Course updated");
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
+
+  const selectedOption = options.find(
+    (option) => option.value === initialData.categoryId,
+  );
+
+  return (
+    <div className="mt-6 rounded-md border bg-slate-100 p-4">
+      <div className="flex items-center justify-between font-medium">
+        Course category
+        <Button variant={"ghost"} onClick={toggleEdditing}>
+          {isEdditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <PenIcon className="mr-2 h-4 w-4" />
+              Edit category
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEdditing && (
+        <p
+          className={cn(
+            "mt-2 text-sm",
+            !initialData.categoryId && "italic text-slate-500 ",
+          )}
+        >
+          {selectedOption?.label || "No category"}
+        </p>
+      )}
+      {isEdditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="mt-4 space-y-4"
+          >
+            <FormField
+              control={form.control}
+              name="categoryId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Combobox options={options} {...field}></Combobox>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Button type="submit" disabled={!isValid || isSubmitting}>
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -8,6 +8,7 @@ import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/TitleForm";
 import { DescriptionForm } from "./_components/DescriptionForm";
 import { ImageForm } from "./_components/ImageForm";
+import { CategoryForm } from "./_components/CategoryForm";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -19,6 +20,12 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const course = await db.course.findUnique({
     where: {
       id: params.courseId,
+    },
+  });
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
     },
   });
 
@@ -56,6 +63,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
           <TitleForm initialData={course} courseId={course.id} />
           <DescriptionForm initialData={course} courseId={course.id} />
           <ImageForm initialData={course} courseId={course.id} />
+          <CategoryForm
+            initialData={course}
+            courseId={course.id}
+            options={categories.map((category) => ({
+              label: category.name,
+              value: category.id,
+            }))}
+          />
         </div>
       </div>
     </div>
