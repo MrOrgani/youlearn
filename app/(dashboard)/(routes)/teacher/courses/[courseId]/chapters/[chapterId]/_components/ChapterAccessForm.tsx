696d6bba4154f733c7b9d749745686acc597b3b7
Maxime Organi <maxime.organi@gmail.com>
2023-12-26 14:32:55 +0100

    Adding chapter acces, free or not

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterAccessForm.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import { Editor } from "@/components/Editor";
+import { Preview } from "@/components/Preview";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Chapter } from "@prisma/client";
+import axios from "axios";
+import { Check, PenIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import * as z from "zod";
+
+interface ChapterAccessFormProps {
+  initialData: Chapter;
+  courseId: string;
+  chapterId: string;
+}
+
+const formSchema = z.object({
+  isFree: z.boolean().default(false),
+});
+
+export const ChapterAccessForm = ({
+  initialData,
+  courseId,
+  chapterId,
+}: ChapterAccessFormProps) => {
+  const [isEdditing, setIsEdditing] = useState(false);
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { isFree: initialData?.isFree || false },
+  });
+
+  const toggleEdditing = () => setIsEdditing((prev) => !prev);
+
+  const { isValid, isSubmitting } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(
+        `/api/courses/${courseId}/chapters/${chapterId}`,
+        values,
+      );
+      toggleEdditing();
+      toast.success("Chapter updated");
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <div className="mt-6 rounded-md border bg-slate-100 p-4">
+      <div className="flex items-center justify-between font-medium">
+        Chapter access
+        <Button variant={"ghost"} onClick={toggleEdditing}>
+          {isEdditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <PenIcon className="mr-2 h-4 w-4" />
+              Edit access
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEdditing && (
+        <p
+          className={cn(
+            "mt-2 text-sm",
+            !initialData.isFree && "italic text-slate-500",
+          )}
+        >
+          {initialData.isFree ? (
+            <>This chapter is free</>
+          ) : (
+            <>This chapter is not free</>
+          )}
+        </p>
+      )}
+      {isEdditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="mt-4 space-y-4"
+          >
+            <FormField
+              control={form.control}
+              name="isFree"
+              render={({ field }) => (
+                <FormItem className="flex items-start space-x-3 space-y-0 rounded-md border p-4">
+                  <FormControl>
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <div className=" space-y-1 leading-none">
+                    <FormDescription>Make it free ?</FormDescription>
+                  </div>
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Button type="submit" disabled={!isValid || isSubmitting}>
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,11 +1,12 @@
 import { IconBadge } from "@/components/IconBadge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { ArrowLeft, LayoutDashboard } from "lucide-react";
+import { ArrowLeft, Eye, LayoutDashboard } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { ChapterTitleForm } from "./_components/ChapterTitleForm";
 import { ChapterDescriptionForm } from "./_components/ChapterDescriptionForm";
+import { ChapterAccessForm } from "./_components/ChapterAccessForm";
 
 const ChapterIdPage = async ({
   params,
@@ -72,6 +73,17 @@ const ChapterIdPage = async ({
             courseId={params.courseId}
           />
           <ChapterDescriptionForm
+            initialData={chapter}
+            chapterId={params.chapterId}
+            courseId={params.courseId}
+          />
+        </div>
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={Eye} />
+            <h2 className="text-xl">Settings</h2>
+          </div>
+          <ChapterAccessForm
             initialData={chapter}
             chapterId={params.chapterId}
             courseId={params.courseId}
