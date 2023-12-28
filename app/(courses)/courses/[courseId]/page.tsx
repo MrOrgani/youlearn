085616b89a324609ebc52f0df8b2dbea7e529f25
Maxime Organi <maxime.organi@gmail.com>
2023-12-28 14:54:48 +0100

    automatic redirect to first chapter after clicking on one course

--- app/(courses)/courses/[courseId]/page.tsx
+++ app/(courses)/courses/[courseId]/page.tsx
@@ -1,7 +1,40 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const CoursePage = () => {
-  return <div>CoursePage</div>;
+const CoursePage = async ({ params }: { param: { courseId: string } }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true,
+          courseId: params.courseId,
+        },
+        include: {
+          userProgress: {
+            where: {
+              userId,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  return redirect(`/courses/${course.id}/chapters/${course.chapters[0].id}`);
 };
 
 export default CoursePage;
