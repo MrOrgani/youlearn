e2a7697b445379eaeccc1fddcce7944c2a938c2c
Maxime Organi <maxime.organi@gmail.com>
2024-01-03 21:45:46 +0100

    css sidebar

--- app/(dashboard)/_components/Sidebar.tsx
+++ app/(dashboard)/_components/Sidebar.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { SidebarRoutes } from "./SidebarRoutes";
+import { Separator } from "@/components/ui/separator";
 
 export const Sidebar = () => {
   return (
-    <div className="flex h-full flex-col overflow-y-auto border-r bg-background">
+    <div className="flex h-full flex-col overflow-y-auto bg-background">
       <div className="flex w-full flex-col bg-background">
         <SidebarRoutes />
+        <Separator orientation="horizontal" className="bg-background20" />
       </div>
     </div>
   );
--- app/(dashboard)/_components/SidebarItem.tsx
+++ app/(dashboard)/_components/SidebarItem.tsx
@@ -28,24 +28,14 @@ export const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
       onClick={onClick}
       type="button"
       className={cn(
-        `flex items-center gap-x-2 pl-6 text-sm font-[500] text-slate-500 transition-all hover:bg-slate-300/20 hover:text-slate-600`,
-        isActive &&
-          "bg-sky-200/20 text-sky-700  hover:bg-sky-200/20 hover:text-sky-700",
+        `hover:bg-background20 flex h-10 items-center gap-x-2 rounded-xl  text-sm font-[500]  text-primary transition-all `,
+        isActive && " bg-background20",
       )}
     >
-      <div className="flex items-center gap-x-2 py-4">
-        <Icon
-          size={20}
-          className={cn("text-slate-500", isActive && "text-sky-700")}
-        />
+      <div className="flex items-center gap-x-2 px-3 ">
+        <Icon size={20} className={cn("", isActive && "")} />
         {label}
       </div>
-      <div
-        className={cn(
-          "ml-auto h-full border-2 border-sky-700 opacity-0 transition-all",
-          isActive && "opacity-100",
-        )}
-      />
     </button>
   );
 };
